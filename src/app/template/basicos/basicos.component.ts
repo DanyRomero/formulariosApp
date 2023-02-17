365b34850ab0e116e619b05d60ee3dc51f8fcdfa
Daniela Romero <danyrp45@gmail.com>
2023-02-17 16:29:03 -0600

    add price validations

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,22 +1,30 @@
 import { Component, Input, Output, ViewChild } from '@angular/core';
-import {NgForm} from '@angular/forms'
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
-  styleUrls: ['./basicos.component.css']
+  styleUrls: ['./basicos.component.css'],
 })
 export class BasicosComponent {
+  @ViewChild('miFormulario') miFormulario!: NgForm;
 
-  @ViewChild('miFormulario') miFormulario! : NgForm;
-
- /*  guardar(miFormualrio: NgForm){ */
- guardar(){
-    console.log(this.miFormulario)
+  /*  guardar(miFormualrio: NgForm){ */
+  guardar() {
+    console.log(this.miFormulario);
   }
 
-  nombreValido(): boolean{
-    return this.miFormulario?.controls['producto']?.invalid &&
-    this.miFormulario?.controls['producto']?.touched
+  nombreValido(): boolean {
+    return (
+      this.miFormulario?.controls['producto']?.invalid &&
+      this.miFormulario?.controls['producto']?.touched
+    );
+  }
+
+  precioValido(): boolean {
+    return (
+      this.miFormulario?.controls['precio']?.value < 0 &&
+      this.miFormulario?.controls['precio']?.touched
+    );
   }
 }
