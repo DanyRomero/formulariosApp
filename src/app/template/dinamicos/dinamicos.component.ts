ad29c9ba921091b479710dfa5b7016388173cfdb
Daniela Romero <danyrp45@gmail.com>
2023-02-27 14:18:26 -0600

    add delete button to favorites on dinamic form

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,6 +1,16 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
@@ -9,9 +19,25 @@ import { NgForm } from '@angular/forms';
 export class DinamicosComponent {
   @ViewChild('miFormulario') miFormulario!: NgForm;
 
+  persona: Persona = {
+    nombre: 'Dany',
+    favoritos: [
+      {
+        id: 1,
+        nombre: 'Shakira',
+      },
+      {
+        id: 2,
+        nombre: 'Dua Lipa',
+      },
+    ],
+  };
+
+  agregar(){}
+  eliminar(i: number){
+    this.persona.favoritos.splice(i, 1)
+  }
   guardar() {
-    
-      console.log('Form posteado.');
-    
+    console.log('Form posteado.');
   }
 }
