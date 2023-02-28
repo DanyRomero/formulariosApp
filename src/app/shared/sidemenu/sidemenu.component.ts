c8d87c1b3cefa9875c7d3a87359141a73d5aebf0
Daniela Romero <danyrp45@gmail.com>
2023-02-28 13:19:21 -0600

    add switches form structure

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,45 +1,48 @@
 import { Component } from '@angular/core';
 
-interface MenuItem{
-  texto: string,
-  ruta: string,
+interface MenuItem {
+  texto: string;
+  ruta: string;
 }
 
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
-  styleUrls: ['./sidemenu.component.css']
+  styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `,
+  ],
 })
 export class SidemenuComponent {
-
   templateMenu: MenuItem[] = [
     {
       texto: 'Básicos',
-      ruta: './template/basicos'
+      ruta: './template/basicos',
     },
     {
       texto: 'Dinámicos',
-      ruta: './template/dinamicos'
+      ruta: './template/dinamicos',
     },
     {
       texto: 'Switches',
-      ruta: './template/switches'
+      ruta: './template/switches',
     },
-  ]
+  ];
   reactiveMenu: MenuItem[] = [
     {
       texto: 'Básicos',
-      ruta: './reactive/basicos'
+      ruta: './reactive/basicos',
     },
     {
       texto: 'Dinámicos',
-      ruta: './reactive/dinamicos'
+      ruta: './reactive/dinamicos',
     },
     {
       texto: 'Switches',
-      ruta: './reactive/switches'
+      ruta: './reactive/switches',
     },
-  ]
-
-
+  ];
 }
