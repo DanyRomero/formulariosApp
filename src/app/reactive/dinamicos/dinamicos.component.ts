ef22337a78a8e790344cacb1838add54942c8fa5
Daniela Romero <danyrp45@gmail.com>
2023-03-01 12:01:05 -0600

    add adding functionality on dinamic forms

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -15,12 +15,23 @@ export class DinamicosComponent {
     ], Validators.required)
   })
 
+
+  nuevoFavorito:FormControl= this.fb.control('', Validators.required)
   constructor( private fb: FormBuilder){}
 
 
   campoValido(campo: string){
     return (this.miFormulario.controls[campo].errors &&
       this.miFormulario.controls[campo].touched)
+  }
+
+  agregarFavorito(){
+   if( this.nuevoFavorito.invalid){
+    return
+  }
+
+  this.favoritosArr.push(this.fb.control(this.nuevoFavorito.value, Validators.required))
+  this.nuevoFavorito.reset()
   }
 
   guardar(){
