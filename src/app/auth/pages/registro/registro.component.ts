2a83634ed214642987739c985137c68b332b7185
Daniela Romero <danyrp45@gmail.com>
2023-03-02 12:06:22 -0600

    add personal validator

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -9,18 +9,31 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class RegistroComponent implements OnInit {
   nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
   emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  
+  
+  noPuedeSerStrider(control: FormControl){
+    const valor = control.value?.trim().toLowerCase();
+    if(valor === 'strider'){
+      return{ noStrider: true}
+    }
+    return null
+    
+  }
+
   miFormulario = this.fb.group({
     nombre: [
       '',
       [Validators.required, Validators.pattern(this.nombreApellidoPattern)],
     ],
     email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+    username: ['', [Validators.required, this.noPuedeSerStrider]],
   });
 
   ngOnInit(): void {
     this.miFormulario.reset({
       nombre: 'Dany Romero',
-      email: 'test1@test.com'
+      email: 'test1@test.com',
+      username: 'Danyrp45',
     });
   }
   constructor(private fb: FormBuilder) {}
